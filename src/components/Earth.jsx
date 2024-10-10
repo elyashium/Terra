75f8caa77c61d4a1dd248a2a43eb3dcefdbad421
Shash <ashishsinghlogic@gmail.com>
2024-10-10 22:45:34 +0530

    added jsx, component still not loading

---
+++ src/components/Earth.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+export default function hero() {
+  return (
+    
+        <Spline scene="https://prod.spline.design/i0RBgD3HEP-icA7X/scene.splinecode" />
+  
+  )
+}

1e1ddcc608e1581976be0b295020c059949f0b10
Shash <ashishsinghlogic@gmail.com>
2024-11-30 22:30:31 +0530

    added learn, timeline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 export default function Header() {
+
+    const navigate = useNavigate();
+
+    
+
     return (
         <div className="header">
            <div className="home">Home</div> 

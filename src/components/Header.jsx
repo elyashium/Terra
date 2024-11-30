cdb5ae61af05ba5cf18c1440a3b19c1190a9eb52
Shash <ashishsinghlogic@gmail.com>
2024-11-30 23:42:40 +0530

    scroll-react
    used the element structure from react- scroll instead of the div to get the scroll function work.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,48 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import '../app.css';
+import React from 'react'
+import { Link } from 'react-scroll'
+import '../app.css'
 
 export default function Header() {
-    return (
-        <header className="header">
-            <nav>
-                <Link
-                    to="home"
-                    smooth={true}
-                    duration={500}
-                    offset={-70}
-                    className="nav-link"
-                >
-                    Home
-                </Link>
-                <Link
-                    to="learn"
-                    smooth={true}
-                    duration={500}
-                    offset={-70}
-                    className="nav-link"
-                >
-                    Learn
-                </Link>
-                <Link
-                    to="timeline"
-                    smooth={true}
-                    duration={500}
-                    offset={-70}
-                    className="nav-link"
-                >
-                    Timeline
-                </Link>
-                <Link
-                    to="events"
-                    smooth={true}
-                    duration={500}
-                    offset={-70}
-                    className="nav-link"
-                >
-                    Events
-                </Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <nav>
+        <Link 
+          to="home" 
+          smooth={true} 
+          duration={500} 
+          offset={-70}
+          className="nav-link"
+        >
+          Home
+        </Link>
+        <Link 
+          to="timeline" 
+          smooth={true} 
+          duration={500} 
+          offset={-70}
+          className="nav-link"
+        >
+          Timeline
+        </Link>
+        <Link 
+          to="learn" 
+          smooth={true} 
+          duration={500} 
+          offset={-70}
+          className="nav-link"
+        >
+          Learn
+        </Link>
+        <Link 
+          to="events" 
+          smooth={true} 
+          duration={500} 
+          offset={-70}
+          className="nav-link"
+        >
+          Events
+        </Link>
+      </nav>
+    </header>
+  )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Element } from 'react-scroll'
 import Header from './Header'
 import Earth from './Earth'
 import { useNavigate } from 'react-router-dom'
@@ -7,7 +8,6 @@ import Timeline from './Timeline'
 import '../app.css'
 
 export default function Hero() {
-
   const navigate = useNavigate();
 
   const handleBtn = () => {
@@ -16,38 +16,42 @@ export default function Hero() {
 
   return (
     <>
-      <main>
-
+      <Element name="home" className="main-content">
         <Header />
 
         <div className="firstContainer">
-
           <div className="text">
-
             <div className="title">terra.</div>
 
             <p>Use our interactive calculator to learn your carbon footprint and actions to take to reduce it.</p>
 
             <div onClick={handleBtn} className="tryit">
-
               <div className="btn">Try it out</div>
               <img src="src\images\arrow.svg" alt="" />
-
             </div>
-
           </div>
 
           <div className="earth">
             <Earth />
           </div>
         </div>
+      </Element>
 
-        <div className="dark-mode-app">
-          <Timeline />
-        </div>
-        
-      </main>
+      <Element name="timeline" className="dark-mode-app">
+        <Timeline />
+      </Element>
 
+      <Element name="learn" className="learn-section">
+        {/* You can add a Learn section content here */}
+        <h2>Learn More About Carbon Footprint</h2>
+        {/* Add any learning resources or information */}
+      </Element>
+
+      <Element name="events" className="events-section">
+        {/* You can add an Events section content here */}
+        <h2>Environmental Events and Initiatives</h2>
+        {/* Add any events or initiatives information */}
+      </Element>
     </>
   )
 }

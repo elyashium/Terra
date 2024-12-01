2b5f79fbf55095bd7e22a382f9f93125a3917728
Shash <ashishsinghlogic@gmail.com>
2024-12-01 23:24:28 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,64 +5,75 @@ import '../app.css'
 export default function Header() {
     return (
         <header className="header">
-            
-                <div>
+            <Link
+                to="home"
+                smooth={true}
+                duration={500}
+                offset={-70}
+                className="nav-link"
+            >
 
-                    <Link
-                        to="home"
-                        smooth={true}
-                        duration={500}
-                        offset={-70}
-                        className="nav-link"
-                    >
-                        Home
-                    </Link>
-
+                <div className="logo">
+                    <img src="src\images\logo.png" alt="" />
                 </div>
+            </Link>
+            <div>
 
-                <div >
+                <Link
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    className="nav-link"
+                >
+                    Home
+                </Link>
 
-                    <Link
-                        to="timeline"
-                        smooth={true}
-                        duration={500}
-                        offset={-70}
-                        className="nav-link"
-                    >
-                        Timeline
-                    </Link>
+            </div>
 
-                </div>
+            <div >
 
+                <Link
+                    to="timeline"
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    className="nav-link"
+                >
+                    Timeline
+                </Link>
 
-                <div>
-
-                    <Link
-                        to="learn"
-                        smooth={true}
-                        duration={500}
-                        offset={-70}
-                        className="nav-link"
-                    >
-                        Learn
-                    </Link>
-                </div>
-
-                <div>
+            </div>
 
 
-                    <Link
-                        to="events"
-                        smooth={true}
-                        duration={500}
-                        offset={-70}
-                        className="nav-link"
-                    >
-                        Events
-                    </Link>
-                </div>
+            <div>
 
-            
+                <Link
+                    to="learn"
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    className="nav-link"
+                >
+                    Learn
+                </Link>
+            </div>
+
+            <div>
+
+
+                <Link
+                    to="events"
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    className="nav-link"
+                >
+                    Events
+                </Link>
+            </div>
+
+
         </header>
     )
 }

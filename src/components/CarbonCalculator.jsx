4cc582e7a828191085c3f5fde485f197bf4a99ae
Shash <ashishsinghlogic@gmail.com>
2024-12-02 23:05:10 +0530

    Update CarbonCalculator.jsx

--- src/components/CarbonCalculator.jsx
+++ src/components/CarbonCalculator.jsx
@@ -4,6 +4,13 @@ import Header from './Header'
 
 export default function CarbonCalculator() {
   return (
-    <Header/>
+    <>
+    <div>
+      <h2>carbon calculation will done here hehe</h2>
+    </div>
+
+
+
+    </>
   )
 }
